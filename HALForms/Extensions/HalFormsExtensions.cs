c909d202d3f132604286518f944b6f08ff3a58c4
John <22085211+jwmxyz@users.noreply.github.com>
2025-04-22 22:20:28 +0100

    - test

--- HALForms/Extensions/HalFormsExtensions.cs
+++ HALForms/Extensions/HalFormsExtensions.cs
@@ -12,37 +12,7 @@ namespace pawd.CoreLibrary.HalForms.Extensions;
 public static class HalFormsExtensions
 {
     
-    /// <summary>
-    /// Adds a HAL-FORMS template based on a model type and attaches validation errors from the <see cref="ModelStateDictionary"/>.
-    /// </summary>
-    /// <typeparam name="T">The type of the model used to generate the template properties.</typeparam>
-    /// <param name="builder">The <see cref="HalFormsBuilder"/> instance to extend.</param>
-    /// <param name="templateName">The name of the template to be added to the HAL-FORMS document.</param>
-    /// <param name="method">The HTTP method (e.g., PUT, POST) to use for the template.</param>
-    /// <param name="target">The target URI where the form data will be submitted.</param>
-    /// <param name="modelInstance">An instance of the model to extract property values and metadata.</param>
-    /// <param name="modelState">
-    /// The model state dictionary containing validation errors. 
-    /// Errors will be attached under the <c>_errors</c> section of the HAL-FORMS document,
-    /// with support for both field-specific and form-level errors (using the <c>__all__</c> key).
-    /// </param>
-    /// <param name="title">An optional title for the template.</param>
-    /// <returns>The updated <see cref="HalFormsBuilder"/> instance for fluent chaining.</returns>
-
-    public static HalFormsBuilder WithTemplateFromModelAndErrors<T>(
-        this HalFormsBuilder builder,
-        string templateName,
-        HalFormHttpMethod method,
-        string target,
-        T modelInstance,
-        ModelStateDictionary modelState,
-        string? title = null)
-    {
-        builder.WithTemplateFromModel(templateName, method, target, modelInstance, title);
-
-        var errors = modelState.ToHalFormErrors();
-        return builder.WithErrors(templateName, errors);
-    }
+    
     
     /// <summary>
     /// Adds a template to the HAL-FORMS document by reflecting over a model instance
